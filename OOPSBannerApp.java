5bd7e7f6b9110f8cd835de9f64a9962b4ee19e22
Prisha Kanodia <pk8246@srmist.edu.in>
2026-02-23 21:46:16 +0530

    UC1: Printed literal text OOPS to console

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+/**
+ * OOPSBannerApp
+ * 
+ * Use Case 1 (UC1): Print literal text "OOPS" to console.
+ * 
+ * @author Prisha Kanodia
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
