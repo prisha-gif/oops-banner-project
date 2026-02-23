6dbf253617751ae051f8038f082d93176cbec03e
Prisha Kanodia <pk8246@srmist.edu.in>
2026-02-23 20:48:47 +0530

    UC3: Added OOPS banner using String.join()

---
+++ OOPSBannerUC3.java
@@ -0,0 +1,11 @@
+public class OOPSBannerUC3 {
+
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ", "  OOO  ", "  OOO  ", " PPPP  ", " SSSS "));
+        System.out.println(String.join(" ", " O   O ", " O   O ", " P   P ", "S      "));
+        System.out.println(String.join(" ", " O   O ", " O   O ", " PPPP  ", " SSS   "));
+        System.out.println(String.join(" ", " O   O ", " O   O ", " P     ", "    S  "));
+        System.out.println(String.join(" ", "  OOO  ", "  OOO  ", " P     ", " SSSS  "));
+    }
+}
